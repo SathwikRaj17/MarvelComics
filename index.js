3281ae75bceb8d9f4f057440459aa7985d84d21c
Sathwik Raj <sathwikraj003@gmail.com>
2024-01-07 23:10:25 +0530

    Final Push

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ import bodyParser from "body-parser";
 const pt=dirname(fileURLToPath(import.meta.url));
 const app=express();
 
-
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(pt+"/public"))
 
@@ -20,12 +19,11 @@ app.get("/",function(req,res)
     res.sendFile(pt+"/public/index.html",pt+"/public/style.css")
 })
 
-async function chargen(charname) {
+async function  chargen(charname) {
     const url = `https://gateway.marvel.com/v1/public/characters?name=${charname}&ts=1&apikey=b6c3f2617e4c10fa730ba5386e70f97d&hash=ea81d9d362afd16ba3d5d4b01e930ac3`;
     try {
       const response = await fetch(url);
       const data = await response.json();
-      console.log(data.data.results[0].thumbnail); 
       return data; 
     } catch (error) {
       console.error("Error fetching data:", error);
@@ -33,13 +31,22 @@ async function chargen(charname) {
     }
 
   }
-  
-app.post("/set",function(req,res)
+app.post("/set",async function(req,res)
 {
-    const a=req.body["name"];
-    console.log(a);
-    chargen(a);
-    res.sendFile(pt+"/public/new.html",pt+"/public/istyle.css")
+    const charName=req.body["name"];
+    const data=await chargen(charName);
+    console.log(data.data.results[0].thumbnail.path)
+    console.log(`Response Code is ${data.code}`);
+    try {
+      res.render(pt+"/views/new.ejs",{
+        path:data.data.results[0].thumbnail.path,
+        name:data.data.results[0].name,
+        desc:data.data.results[0].description
+      }); 
+    } catch (error) {
+      
+    }
+    
 })
 
 // const pk="b6c3f2617e4c10fa730ba5386e70f97d";
